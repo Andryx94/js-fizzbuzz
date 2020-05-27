7db981177ebf2151531f6c783a2abe667a1096e0
Andrea <andryx1994@gmail.com>
2020-05-27 15:43:48 +0200

    Versione 1.1
    Aggiunto commenti js e ottimizzato la divisione 15

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ var lista = document.getElementById('lista')
 
 //ciclo for
 for (var i=1; i<=100; i++) {
-  if ((i % 3 == 0) && ( i % 5 == 0)) {
+  //se divisibile sia per 5 che per 3
+  if (i % 15 == 0) {
     lista.innerHTML += '<li>' + "FizzBuzz";
   }
 
+  //se divisibile per 3
   else if (i % 3 == 0) {
     lista.innerHTML += '<li>' + "Fizz";
   }
 
+  //se divisibile per 5
   else if ( i % 5 == 0) {
     lista.innerHTML += '<li>' + "Buzz";
   }
